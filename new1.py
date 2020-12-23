49fb091531cfc0092f7b57fae127ad9b4d9da3d1
user <idan.ergaz@gmail.com>
2020-12-23 09:33:31 +0200

    first commit by Idan

---
+++ new1.py
@@ -0,0 +1,8 @@
+print("Hello world!")
+name="Idan"
+age=40
+print(f'your name is {name} and your age is:{age}')
+print("Another way!!!!!!")#####################
+print("#############################################")
+print("your name is:{} and you are {} years old".format(name, age))
+print("adding a NEW text - check auto test run after commit !!!!!")
